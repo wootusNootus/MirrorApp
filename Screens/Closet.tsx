9917c4b2fbbe8f66f7968978aba50d84cd87d50a
wootusNootus <84185710+wootusNootus@users.noreply.github.com>
2023-04-02 21:11:57 -0500

    menu commit 2nd test

--- Screens/Closet.tsx
+++ Screens/Closet.tsx
@@ -5,24 +5,9 @@ import Menu from '../components/Menu';
 const ClosetScreen = () => {
     return <View>
         <Text>Closet Screen</Text>
-        <View style={styles.container}>
-            <Menu />
-        </View>
+        
     </View>
-
 }
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    content: {
-      justifyContent: "center",
-      alignItems: "center",
-      marginTop: 150.
-    },
-  });
 
 export default ClosetScreen
